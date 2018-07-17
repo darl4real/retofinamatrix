e2d5d7f92fcbd62cf19110e99977175a61fe406c
darl4real <darl4real@hotmail.com>
2018-07-17 19:04:06 +0200

    routes/index.js created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const IndexController = require('../controller/web/indexController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    let indexController = new IndexController(req, res, next);
+    indexController.view();
 });
 
 module.exports = router;
