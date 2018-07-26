d998fc7139e6200c3cfb7988def4718d89f7e94e
Darlington <darl692000@yahoo.com>
2018-07-26 15:46:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -22,12 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
+/*// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
-});
+  next(createError(404));
+});*/
 
 // error handler
 app.use(function(err, req, res, next) {
